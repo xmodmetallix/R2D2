e544d66fe284e8b891e7533d2c1a3b3a37887c8d
R2D2 <R2D2@10.30.11.8>
2018-09-13 19:45:16 -0500

    Mi primer proyecto de GitHub

---
+++ src/arturo_amador/Arturo_Amador.java
@@ -0,0 +1,10 @@
+package arturo_amador;
+
+import javax.swing.JOptionPane;
+
+public class Arturo_Amador {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        JOptionPane.showMessageDialog(null,"Hola mundo");
+    }
+}
